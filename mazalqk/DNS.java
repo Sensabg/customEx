93009b5762c0041b49ceb2842a2481e2743e988d
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-11-03 16:07:11 +0200

    Update DNS.java

--- mazalqk/DNS.java
+++ mazalqk/DNS.java
@@ -54,7 +54,6 @@ public class DNS {
         return true;
     }
 
-
     private static String getEntry(int entry) {
         return switch (entry) {
             case Type.A -> "A";
@@ -85,68 +84,3 @@ public class DNS {
         return records != null ? Arrays.asList(records) : Collections.emptyList();
     }
 }
-
-
-//   private static boolean isValid(int entry) {
-//        return switch (entry) {
-//            case Type.A, Type.MX, Type.NS, Type.TXT, Type.SOA, Type.CNAME, 0 -> true;
-//            default -> false;
-//        };
-
-//       String dnsRecord;
-////        try {
-////            if (recordType == 0) {
-////                for (int entry : new int[]{Type.A, Type.MX, Type.NS, Type.TXT, Type.SOA, Type.CNAME}) {
-////
-////                    List<Record> records = getRecords(domain, entry);
-////                    dnsRecord = Type.string(entry);
-////                    System.out.println("\n" + dnsRecord + " records for domain: " + domain);
-////                    records.forEach(record -> System.out.println(record.rdataToString()));
-////                }
-////            } else {
-////                List<Record> records = getRecords(domain, recordType);
-////                System.out.println("\n" + recordType + " for domain: " + domain);
-////                records.forEach(record -> System.out.println(record.rdataToString()));
-////            }
-////        } catch (Exception e) {
-////
-////            System.out.println( dnsRecord + " Record not found for domain: "+ domain);
-////        }
-
-// switch (currentType) {
-//            case Type.A -> type = "A";
-//            case Type.MX -> type = "MX";
-//            case Type.NS -> type = "NS";
-//            case Type.TXT -> type = "TXT";
-//            case Type.SOA -> type = "SOA";
-//            case Type.CNAME -> type = "CNAME";
-//            default -> {
-//                System.out.println(type + "Please enter a valid type");
-//                type = scanner.nextLine();
-//            }
-
-
-//DNS.recordTypes record = DNS.recordTypes.parseString(type);
-//
-//        String currentType = null;
-//
-//        while ("INVALID".equals(currentType)) {
-//              currentType =  switch (record) {
-//                case A -> currentType = "A";
-//                case MX -> currentType = "MX";
-//                case NS -> currentType = "NS";
-//                case TXT -> currentType = "TXT";
-//                case SOA -> currentType = "SOA";
-//                case CNAME -> currentType = "CNAME";
-//                case INVALID -> currentType = scanner.nextLine();
-//            };
-//        }
-
-//private static void doesExist(String domain, int entry) throws TextParseException {
-//        List<Record> records = getRecords(domain, entry);
-//
-//        while (records != null && !records.isEmpty()) {
-//            System.out.println("\n" + entry + " records for domain: " + domain);
-//            records.forEach(record -> System.out.println(record.rdataToString()));
-//        }
-//    }
