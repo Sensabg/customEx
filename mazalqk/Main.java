1b599f00b4150ac68e1ebbbe54830ae4a2f47030
Daniel Dinev <151548889+Sensabg@users.noreply.github.com>
2024-11-03 14:05:21 +0200

    Create Main.java

---
+++ mazalqk/Main.java
@@ -0,0 +1,7 @@
+import org.xbill.DNS.TextParseException;
+
+public class Main {
+    public static void main(String[] args) throws TextParseException {
+        DNS dns = new DNS(new Input());
+    }
+}
